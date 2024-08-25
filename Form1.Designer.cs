bdd4f596a21e39dffafc64def57b372b62f93d32
Gitekkaad <129181365+Gitekkaad@users.noreply.github.com>
2024-08-26 00:55:52 +0200

    commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿
+namespace SnowSniff2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.richTextBox1.Font = new System.Drawing.Font("Cascadia Mono", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.richTextBox1.Location = new System.Drawing.Point(30, 121);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(1686, 397);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(263, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(75, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(201, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "count";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(263, 62);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(75, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(196, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "socalls";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(387, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "ready to sniff";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(1128, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "whois";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(387, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "dns entry";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(30, 68);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 10;
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.richTextBox2.Font = new System.Drawing.Font("Cascadia Mono", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox2.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.richTextBox2.Location = new System.Drawing.Point(30, 582);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(1686, 363);
+            this.richTextBox2.TabIndex = 11;
+            this.richTextBox2.Text = "";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(1416, 27);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(1752, 957);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "raw socket®fbasics software";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.TextBox textBox1;
+        public System.Windows.Forms.TextBox textBox2;
+        public System.Windows.Forms.Label label3;
+        public System.Windows.Forms.Label label4;
+        public System.Windows.Forms.Label label5;
+        public System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Whois.NET;
+
+
+namespace SnowSniff2
+{
+
+    public partial class Form1 : Form
+    {
+        IPHostEntry myip;
+        IPHostEntry daddr;
+        Socket meinsocket;
+
+        int i;
+        int tcpoffset, dataoffset;
+        string destaddr;
+        string sorcaddr;
+        string lastadress;
+
+        StringBuilder zeile = new StringBuilder();
+
+        string umaddr;
+        public byte[] buf;
+
+
+        int ipvers;
+        int headlen;
+        int sport;
+        int dport;
+        int ttl = 1;
+        int proto;
+        int flct;
+        int count;
+        int thcount;
+        int retcount;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.FormClosing += Form1_FormClosing;
+
+            _ = ssnifAsync();
+
+
+        }
+
+        private void Form1_FormClosing(Object sender, FormClosingEventArgs e)
+        {
+
+            System.Text.StringBuilder messageBoxCS = new System.Text.StringBuilder();
+            messageBoxCS.AppendFormat("{0} = {1}", "CloseReason", e.CloseReason);
+            messageBoxCS.AppendLine();
+            messageBoxCS.AppendFormat("{0} = {1}", "Cancel", e.Cancel);
+            messageBoxCS.AppendLine();
+            MessageBox.Show(messageBoxCS.ToString(), "FormClosing Event");
+        }
+
+
+
+
+
+        public async Task ssnifAsync()
+
+        {
+            myip = Dns.GetHostEntry(Dns.GetHostName());
+            for (i = 0; i < myip.AddressList.Length; i++)
+            {
+                if (myip.AddressList[i].AddressFamily ==
+                AddressFamily.InterNetwork)
+                    break;
+            }
+
+            if (i == myip.AddressList.Length) return;
+
+            IPEndPoint endpunkt;
+
+            endpunkt = new IPEndPoint(myip.AddressList[i], 0);
+
+            //Socket meinsocket;
+
+            meinsocket = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.IP);
+
+            meinsocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.HeaderIncluded, true);
+
+            byte[] buf = new byte[meinsocket.ReceiveBufferSize];
+
+            meinsocket.Bind(endpunkt);
+
+            //meinsocket.IOControl(IOControlCode.NonBlockingIO, null, BitConverter.GetBytes(1));
+
+            meinsocket.IOControl(IOControlCode.ReceiveAll, BitConverter.GetBytes(1), null);
+
+           
+
+
+            label3.Text = ("<--- analysing data for user string ");
+            label3.Refresh();
+
+        rms:
+
+            await Task.Run(() => Lesen());
+
+            textBox3.Text = thcount.ToString();
+            textBox3.Refresh();
+
+
+            flct++;
+            textBox2.Text = (flct.ToString());
+            textBox2.Refresh();
+
+            textBox1.Text = count.ToString();
+            textBox1.Refresh();
+
+            if (count != 128) { goto rms; }
+
+            richTextBox1.Update();
+            richTextBox1.AppendText(zeile.ToString());
+            richTextBox1.ScrollToCaret();
+
+            QueryByIPAddress();
+
+            goto rms;
+
+        }
+
+
+        public void QueryByIPAddress()
+        {
+
+            try
+            {
+
+                if (lastadress == sorcaddr)
+                {
+
+
+                    _ = textBox4.Text = retcount.ToString();
+
+                    textBox4.Refresh();
+
+                    retcount++;
+
+                    return;
+                }
+
+                var result = WhoisClient.Query(sorcaddr);
+
+                richTextBox2.Text = result.Raw.ToString();
+                richTextBox2.Update();
+                richTextBox2.ScrollToCaret();
+
+                try
+                {
+
+                    label3.Text = result.OrganizationName.ToString();
+                    label3.Update();
+                    label3.Refresh();
+
+                }
+
+                catch (Exception e) { }
+
+
+                if (Dns.GetHostEntry(sorcaddr.ToString()) != null)
+                {
+
+                    daddr = Dns.GetHostEntry(sorcaddr.ToString());
+                    label5.Text = daddr.HostName;
+                    label5.Refresh();
+
+                }
+
+            }
+
+
+
+            catch (SocketException e)
+            {
+
+                label5.Text = "no dns entry for hostname";
+                label5.Refresh();
+
+            }
+
+            lastadress = sorcaddr.ToString();
+        }
+
+
+        public void Lesen()
+
+        {
+
+            byte[] buf = new byte[meinsocket.ReceiveBufferSize];
+
+            count = meinsocket.Receive(buf);
+
+            thcount++;
+
+            //if (count != 128) { return; }
+
+            if (count != 128 && count != 136) { return; }
+
+                destaddr = buf[16] + "." + buf[17] + "." + buf[18] + "." + buf[19];
+            sorcaddr = buf[12] + "." + buf[13] + "." + buf[14] + "." + buf[15];
+
+            umaddr = buf[12] + "." + buf[13] + "." + buf[14];
+            //if (umaddr == "192.168.178") { goto rms; };
+
+            if (umaddr == "192.168.178") { return; };
+
+            tcpoffset = (buf[0] & 0x0f) * 4;
+            dataoffset = (buf[tcpoffset + 12] >> 4) * 4;
+
+            ipvers = (buf[0] >> 4);
+            headlen = (buf[0] & 0x0f) * 4;
+            sport = (buf[tcpoffset] << 8) + (buf[tcpoffset + 1]);
+            dport = (buf[tcpoffset + 2] << 8) + (buf[tcpoffset + 3]);
+            ttl = buf[8];
+            proto = buf[9];
+
+            DateTime myValue = DateTime.Now;
+            zeile.Append(myValue.ToString());
+
+            zeile.Append(" IPv ");
+            zeile.Append(ipvers.ToString());
+
+            //zeile.Append(" Hl ");
+            //zeile.Append(headlen.ToString());
+
+            zeile.Append(" : ");
+            zeile.Append(sorcaddr.ToString());
+
+            zeile.Append(" : ");
+            zeile.Append(sport.ToString());
+
+            zeile.Append(" : ");
+            zeile.Append(destaddr.ToString());
+
+            zeile.Append(" : ");
+            zeile.Append(dport.ToString());
+
+            zeile.Append("  TTL  ");
+            zeile.Append(ttl.ToString());
+
+            zeile.Append("  Protocol ");
+            if (proto == 17) { zeile.Append(" UDP "); }
+            if (proto == 6) { zeile.Append(" TCP "); }
+
+            zeile.Append(" count -->> ");
+            zeile.Append(count.ToString());
+            zeile.Append(" username  ----->  ");
+
+            // eavesdropper
+
+            for (int b = (tcpoffset + dataoffset + 24); b < count; b++)
+            {
+                if (Char.IsControl(Convert.ToChar(buf[b])))
+                    zeile.Append(".");
+                else
+                    zeile.Append(Convert.ToChar(buf[b]));
+            }
+
+            zeile.Append(" \r\n");
+
+
+
+            
+
+
+
+
+        }
+
+
+
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
